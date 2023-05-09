297fa4630b55c16ca8c1560d7602e8fe8f10ad7d
lirockk <arthurdemurger26@gmail.com>
2023-05-09 17:15:18 +0200

    Work on socket object

--- inc/Socket.hpp
+++ inc/Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:13:50 by ademurge          #+#    #+#             */
-/*   Updated: 2023/05/09 15:48:27 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:59:58 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,29 @@
 class Socket
 {
 	private:
-		int					server_sock;
+		int					serverSock;
 		int					connection;
 		struct sockaddr_in	address;
 
 	public:
+		/*
+		** ------------------------------- Canonical form --------------------------------
+		*/
 		Socket(int domain, int service, int protocol, int port, u_long interface);
-
-		virtual int	connect_to_ntwk() = 0;
-		void		test_connection();
 		~Socket();
+
+		/*
+		** ------------------------------- Accessors --------------------------------
+		*/
+		int					getServerSock(void) const;
+		int					getConnection(void) const;
+		struct sockaddr_in	getAddress(void) const;
+
+		/*
+		** ------------------------------- Methods --------------------------------
+		*/
+		virtual int	connect_to_ntwk() = 0;
+		void		check(int item, char *message);
 };
 
 # endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #define PORT 8080
+
 int main(int argc, char const *argv[])
 {
 	int server_fd, new_socket;
