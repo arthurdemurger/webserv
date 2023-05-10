8af763846ef24521fa734a9aba92f5df6ab16e24
lirockk <arthurdemurger26@gmail.com>
2023-05-10 15:59:53 +0200

    minor change

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:12:06 by ademurge          #+#    #+#             */
-/*   Updated: 2023/05/10 15:21:23 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:56:28 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 int main(void)
 {
-	while (true)
-	{
-		Server serv(AF_INET, SOCK_STREAM, 0, PORT, INADDR_ANY, 10);
+	Server serv(AF_INET, SOCK_STREAM, 0, PORT, INADDR_ANY, 10);
 
-		serv.launcher();
-	}
+	serv.launcher();
 	return (0);
 }
