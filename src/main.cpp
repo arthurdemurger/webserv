2f598822c533ef7d84bab43d2236dbc4cc9a9487
lirockk <arthurdemurger26@gmail.com>
2023-05-25 16:55:46 +0200

    fix compiling problem

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:12:06 by ademurge          #+#    #+#             */
-/*   Updated: 2023/05/25 16:35:09 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/05/25 16:37:15 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 		std::string conf_filename = "";
 
 		if (ac == 2)
-			conf_filename == av[1];
+			conf_filename = av[1];
 
 		Launcher	launcher(conf_filename);
 
